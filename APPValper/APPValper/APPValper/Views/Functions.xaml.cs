b10deaa73339cfd52bb0d70090f303e88644a19b
Gonzalo <gonzag0139@gmail.com>
2020-02-10 19:46:34 +0000

    MainMenuFinish

---
+++ APPValper/APPValper/APPValper/Views/Functions.xaml.cs
@@ -0,0 +1,43 @@
+﻿using APPValper.Models;
+using APPValper.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APPValper.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Functions : ContentPage
+    {
+        FunctionsViewModel Context = new FunctionsViewModel();
+        public Functions()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            BindingContext = Context;
+            LvFunctions.ItemSelected += LvFunctions_ItemSelected;
+        }
+
+        private void LvFunctions_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                Function model = (Function)e.SelectedItem;
+                if (Context.GoBool)
+                {
+                    ((ListView)sender).SelectedItem = null;
+                    //Navigation.PushAsync(new DetallePage(model));
+                }
+                Context.Server = model.Server;
+                Context.Action = model.Action;
+                Context.Description = model.Description;
+                Context.Id = model.Id;
+            }
+        }
+    }
+}
