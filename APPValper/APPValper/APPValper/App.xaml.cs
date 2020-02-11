aa31033f9b4e378fd852ba1036e783557be4444e
David <doriveramirez@gmail.com>
2020-02-11 17:36:35 +0000

    next

--- APPValper/APPValper/APPValper/App.xaml.cs
+++ APPValper/APPValper/APPValper/App.xaml.cs
@@ -14,7 +14,7 @@ namespace APPValper
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
